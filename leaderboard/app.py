15b41f0a723f4b8a970b8ebd57a91863f0f29399
Bushra Farhad <116449605+VforViolet10@users.noreply.github.com>
2026-04-13 20:01:59 +0530

    Update and rename app.py to streamlit_app.py

--- leaderboard/app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-st.title(" Graph Robustness Leaderboard")
-
-df = pd.read_csv("leaderboard/leaderboard.csv")
-
-st.dataframe(df, use_container_width=True)
