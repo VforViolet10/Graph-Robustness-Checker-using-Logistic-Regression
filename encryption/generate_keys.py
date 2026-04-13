c9cd735668c1eddb85beaaa9274c23e2153caaf9
joyashaikh924 <joyashaikh0924@gmail.com>
2026-04-13 14:29:41 +0530

    Update generate_keys.py

--- encryption/generate_keys.py
+++ encryption/generate_keys.py
@@ -1,1 +1,34 @@
+# encryption/generate_keys.py
 
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+
+# Generate private key
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
+)
+
+# Convert private key to PEM
+private_pem = private_key.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.PKCS8,
+    encryption_algorithm=serialization.NoEncryption()
+)
+
+# Generate public key
+public_key = private_key.public_key()
+
+public_pem = public_key.public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+)
+
+# Save keys
+with open("private_key.pem", "wb") as f:
+    f.write(private_pem)
+
+with open("public_key.pem", "wb") as f:
+    f.write(public_pem)
+
+print("✅ Keys generated!")
